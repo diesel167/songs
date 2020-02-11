24a3025abbabecdc6b3813989a7f86599ad1aebf
Yury Misiyevich <misiyevich@gmail.com>
2020-02-11 14:24:06 +0300

    feat: add sorting for column titles

--- src/Components/Songs.js
+++ src/Components/Songs.js
@@ -7,7 +7,8 @@ class Songs extends Component {
         super(props);
         this.state = {
             songs: '',
-            isAllSongsLoaded: false
+            isAllSongsLoaded: false,
+            sorted: ''
         };
     }
 
@@ -15,7 +16,7 @@ class Songs extends Component {
         fetch('https://api.myjson.com/bins/sml9c')
             .then(response => response.json())
             .then((a)=>{
-                this.setState({songs: a.songs});
+                this.setState({songs: [...a.songs]});
                 this.setState({isAllSongsLoaded: true});
                 this.props.howManyPages(a.songs.length);
             });
@@ -28,8 +29,7 @@ class Songs extends Component {
         if(this.state.isAllSongsLoaded){
             let start = this.props.currentPage*this.props.songsPerPage-this.props.songsPerPage;
             let end = this.props.currentPage*this.props.songsPerPage > this.state.songs.length ? this.state.songs.length : this.props.currentPage*this.props.songsPerPage;
-            console.log(end);
-            [...this.state.songs].slice(start,end).map(song => {
+            this.state.songs.slice(start,end).map(song => {
                 let row=<tr>
                     <td>{song.artist}</td>
                     <td>{song.title}</td>
@@ -42,15 +42,89 @@ class Songs extends Component {
     };
 
     sortBySinger=()=>{
-        let sorted = {};
         let unsorted = this.state.songs;
-        console.log(Object.keys(unsorted));
-        /*Object.keys(unsorted).sort().forEach(function(key) {
-            sorted[key] = unsorted[key];
-        });
-        this.setState({songs: sorted})*/
-    }
-
+        if(this.state.sorted === 'singerAsc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'singerDesc'
+            })
+        }
+        else if(this.state.sorted === 'singerDesc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'singerAsc'
+            })
+        }
+        else{
+            unsorted.sort(function(a, b){
+                if(a.artist < b.artist) { return -1; }
+                if(a.artist > b.artist) { return 1; }
+                return 0;
+            })
+            this.setState({
+                songs: unsorted,
+                sorted: 'singerAsc'
+            })
+        }
+    };
+    sortBySong=()=>{
+        let unsorted = this.state.songs;
+        if(this.state.sorted === 'songAsc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'songDesc'
+            })
+        }
+        else if(this.state.sorted === 'songDesc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'songAsc'
+            })
+        }
+        else{
+            unsorted.sort(function(a, b){
+                if(a.title < b.title) { return -1; }
+                if(a.title > b.title) { return 1; }
+                return 0;
+            })
+            this.setState({
+                songs: unsorted,
+                sorted: 'songAsc'
+            })
+        }
+    };
+    sortByYear=()=>{
+        let unsorted = this.state.songs;
+        if(this.state.sorted === 'yearAsc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'yearDesc'
+            })
+        }
+        else if(this.state.sorted === 'yearDesc'){
+            unsorted.reverse();
+            this.setState({
+                songs: unsorted,
+                sorted: 'yearAsc'
+            })
+        }
+        else{
+            unsorted.sort(function(a, b){
+                if(a.year < b.year) { return -1; }
+                if(a.year > b.year) { return 1; }
+                return 0;
+            })
+            this.setState({
+                songs: unsorted,
+                sorted: 'yearAsc'
+            })
+        }
+    };
 
     render() {
         return (
@@ -59,8 +133,8 @@ class Songs extends Component {
                     <thead>
                         <tr>
                             <th onClick={this.sortBySinger}>Singer</th>
-                            <th>Song</th>
-                            <th>Year</th>
+                            <th onClick={this.sortBySong}>Song</th>
+                            <th onClick={this.sortByYear}>Year</th>
                         </tr>
                     </thead>
                     <tbody>
