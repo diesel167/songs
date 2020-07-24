f46c4aacd9333d95f0a62cede0a0b3cf9d66d8fd
Yury Misiyevich <misiyevich@gmail.com>
2020-07-24 15:26:03 +0300

    feat:build

---
+++ precache-manifest.15c454ec2a901b2395bdcd09335f33a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "894392417256bdc8c3c838897fde81e0",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "ce8620f47c440c502f45",
+    "url": "/songs/static/css/main.1d1e8602.chunk.css"
+  },
+  {
+    "revision": "d6f486cf85e75498e87d",
+    "url": "/songs/static/js/2.bfa7395a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/songs/static/js/2.bfa7395a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8620f47c440c502f45",
+    "url": "/songs/static/js/main.28d61765.chunk.js"
+  },
+  {
+    "revision": "985d722f8c94a17b32a5",
+    "url": "/songs/static/js/runtime-main.0178b6a0.js"
+  }
+]);
